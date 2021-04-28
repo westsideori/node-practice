cf4bff996e4b61e211f077a576a9b8801a21071f
Ori Markowitz <ori.markowitz@gmail.com>
2021-04-28 18:56:41 -0400

    refactored addNote

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -7,11 +7,9 @@ const getNotes = () => {
 
 const addNote = (title, body) => {
   const notes = loadNotes();
-  const duplicateNotes = notes.filter((note) => {
-    return note.title === title;
-  });
+  const duplicateNote = notes.find((note) => note.title === title);
 
-  if (duplicateNotes.length === 0) {
+  if (!duplicateNote) {
     notes.push({
       title: title,
       body: body,
