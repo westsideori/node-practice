832eb4f9d965c2c95235fd2e163f3f9259b5430a
Ori Markowitz <ori.markowitz@gmail.com>
2021-04-29 16:09:58 -0400

    added web-server folders

---
+++ web-server/src/app.js
@@ -0,0 +1,14 @@
+const path = require("path");
+const express = require("express");
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "../public")));
+
+app.get("/weather", (req, res) => {
+  res.send({ forecast: "Overcast", location: "NYC" });
+});
+
+app.listen(3000, () => {
+  console.log("Server is up on port 3000");
+});
